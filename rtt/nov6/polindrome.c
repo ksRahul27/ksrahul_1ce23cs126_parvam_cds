b9a3c9d78fc51409dd529d312dd16dc2adc13e3e
ksRahul27 <attitudeboy2577@gmail.com>
2024-11-06 13:04:40 +0530

    nov6

---
+++ rtt/nov6/polindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the string")
+    scanf("%d",&n);
+    int r=0;
+    while(n!=0){
+        int rem=n%10
+        r=r*10+rem;
+        n=n/10;
+    }
+    if(r==temp){
+        printf("%d",r);
+    }
+    else{
+        printf("polindrome");
+    }
+    
+}
